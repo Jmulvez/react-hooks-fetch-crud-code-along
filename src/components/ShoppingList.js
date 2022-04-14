f6db2648ba0a8e5fce5fe937a25555701b21ff3e
John <johnmulvey48@yahoo.com>
2022-04-14 11:30:39 -0400

    git@github.com:Jmulvez/react-hooks-fetch-crud-code-along.git

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useWffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,26 +7,27 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleUpdateItem(updatedItem) {
+    console.log("In ShoppingCart:", updatedItem);
   }
-
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
+        {}
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
